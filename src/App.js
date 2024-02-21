0d140a1fde2a3b3edeba37b7c18ec447fbca0e25
Johncxsong <chenxisong@hotmail.com>
2024-02-21 16:32:59 -0500

    page modules structure bulding

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+// dependencies
+import React from 'react';
+
+// content module import
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Aside from './components/Aside';
+import Section from './components/Section';
+import Article from './components/Article';
+import Footer from './components/Footer';
+
+// styling
 import './App.css';
+
 
 function App() {
   return(
   <>
     <Header />
     <Nav />
-    <Main>
+    <main>
       <Aside />
       <Section>
         <Article />
       </Section>
-    </Main>
+    </main>
     <Footer />
   </>
   );
